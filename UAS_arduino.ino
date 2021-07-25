425f53eed672db4d440935eb09e0672ad0809a6b
GazicAditya <54955092+GazicAditya@users.noreply.github.com>
2021-07-25 19:43:22 +0700

    Update UAS_arduino.ino

--- UAS_arduino.ino
+++ UAS_arduino.ino
@@ -1,20 +1,3 @@
-/*
-Cayenne 2 State Widget Example
-
-This sketch shows how to set up a 2 State Custom Widget with Cayenne.
-
-The CayenneMQTT Library is required to run this sketch. If you have not already done so you can install it from the Arduino IDE Library Manager.
-
-Steps:
-1. Attach a digital input device (e.g. a button) to a digital pin on your Arduino.
-2. Set the SENSOR_PIN value below to the pin number you used when connecting the sensor.
-3. Set the VIRTUAL_CHANNEL value below to a free virtual channel (or the virtual channel of a 2 State Custom Widget you have added) in the Dashboard.
-4. Set the Cayenne authentication info to match the authentication info from the Dashboard.
-5. Compile and upload this sketch.
-6. Once the Arduino connects to the Dashboard it should automatically create a temporary display widget (or update the 2 State widget you have added) with data.
-   To make a temporary widget permanent click the plus sign on the widget.
-*/
-
 #define CAYENNE_PRINT Serial  // Comment this out to disable prints and save space
 #include <CayenneMQTTEthernet.h>
 #include <TinyGPS++.h>
